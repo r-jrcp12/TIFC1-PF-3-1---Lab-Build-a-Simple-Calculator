a41d253d8b07a341727a9979545619d66062ad6f
r-jrcp12 <tapiaaraujoramon@gmail.com>
2026-02-06 20:26:10 -0500

    calculadora

---
+++ main.py
@@ -0,0 +1,16 @@
+num_1 = float(input("Dame un numero: "))
+num_2 = float(input("Dame un numero: "))
+
+
+suma = num_1 + num_2 
+resta = num_1 - num_2 
+multi = num_1 * num_2 
+division = num_1 / num_2 
+
+print(f"{suma}: suma ")
+print(f"{resta}: resta")
+print(f"{multi}: multiplicacion")
+print(f"{division}: divison")
+
+
+
